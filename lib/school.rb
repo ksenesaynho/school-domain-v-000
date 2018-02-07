e4a45d5d2dca71116f3d8002dcaf5acfe3bdb1fc
Kristina Senesaynho <ksenesaynho@gmail.com>
2018-02-07 18:07:53 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,5 +3,12 @@ class School
   def intialized(name)
     @name = name
     @roster = {}
+  end
   
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+  
+  def grade_
 end
